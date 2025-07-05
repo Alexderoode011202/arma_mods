90201be9ecc62bcd78495bd9c480a70495bffd06
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2025-07-05 03:03:38 +0200

    added path extraction logic

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from tkinter import Tk, Label, Entry, Button
+from tkinter.filedialog import askopenfilename
 from typing import List, Tuple, Set, Dict
 from functions import fully_process
+import os
 
+first_path: str 
+second_path: str
 
-data: Dict[str, List[Tuple[str, str]]]
-
+def extract_path(path: int = 0) -> str:
+    global first_path, second_path, first_file_label, second_file_label
     
+    file_path: str = askopenfilename(initialdir=os.getcwd(), 
+                                       title="get your mod file", 
+                                       filetypes=(("HTML files", "*.html"), 
+                                                  ("all files", "*.*")))
+    if path == 0:
+        first_path = file_path
+        first_file_label.config(text=f"file: {file_path}")    
+    elif path == 1:
+        second_path = file_path
+        second_file_label.config(text=f"file: {file_path}")    
+    
+
+
+
 root = Tk()
 root.title("mod comparer")
 root.geometry("1000x500")
 
-first_file_button   : Button = Button(root, text="find first file:", width=20)
-second_file_button  : Button = Button(root, text="find secondt file:", width=20)
+first_file_button   : Button = Button(root, text="find first file:"  , width=20, command=lambda : extract_path(path=0))
+second_file_button  : Button = Button(root, text="find secondt file:", width=20, command=lambda : extract_path(path=1))
 
 first_file_label    : Label = Label(root)
 second_file_label   : Label = Label(root)
