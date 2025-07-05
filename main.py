a070aa51d8a25af23c453d4a6617661be4208685
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2025-07-05 02:54:36 +0200

    got rid of useless function in main.py

--- main.py
+++ main.py
@@ -4,33 +4,7 @@ from functions import fully_process
 
 
 data: Dict[str, List[Tuple[str, str]]]
-def test_click(*args) -> None:
-    global root, data
-    OWN_FILE: str = args[0]
-    OTHER_FILE: str = args[1]
-    
-    data = fully_process(OWN_FILE, OTHER_FILE)
-    
-    own_only: List[Tuple[str, str]] = data["first only"]
-    other_only: List[Tuple[str, str]] = data["second only"]
-    
-    own_labels: List[Label] = [Label(root, text=f"{mod[0]}") for mod in own_only]
-    other_labels: List[Label] = [Label(root, text=f"{mod[0]}") for mod in other_only]
-    
-    own_label: Label = Label(root, text=f"{OWN_FILE}'s exclusive mods:")
-    other_label: Label = Label(root, text=f"{OTHER_FILE}'s exclusive mods:")
-    
-    own_label.grid(column=0, row=2)
-    other_label.grid(column=0, row=3)
-    
-    for idx, label in enumerate(own_labels):
-        label.grid(column=idx+1, row=2)
-        
-    for idx, label in enumerate(other_labels):
-        label.grid(column=idx+1, row=3)
-        
-    
-    return None
+
     
 root = Tk()
 root.title("mod comparer")
